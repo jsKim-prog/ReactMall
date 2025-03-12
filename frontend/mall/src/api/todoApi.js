4c7428f34f648dbb101335a2198c1088df77540f
SARONY\sarony <jscode77@gmail.com>
2025-03-12 20:53:06 +0900

    component, api, Custom hook, Axios, useEffect

---
+++ frontend/mall/src/api/todoApi.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+export const API_SERVER_HOST = 'http://localhost:8080'
+const prefix = `${API_SERVER_HOST}/api/todo`
+// 조회
+export const getOne = async (tno) =>{
+    const res = await axios.get(`${prefix}/${tno}`)
+    return res.data
+}
+// 리스트조회
+export const getList = async (pageParam) =>{
+    const {page, size} = pageParam
+    const res = await axios.get(`${prefix}/list`,{params:{page:page, size:size}})
+    return res.data
+}
+
+//등록(post)
+export const postAdd = async (todoObj) => {
+    const res = await axios.post(`${prefix}/`, todoObj)
+    return res.data
+}
+
+//수정(put)
+export const putOne = async (todo) =>{
+    const res = await axios.put(`${prefix}/${todo.tno}`, todo)
+    return res.data
+}
+
+//삭제(delete)
+export const deleteOne = async (tno) =>{
+    const res = await axios.delete(`${prefix}/${tno}`)
+    return res.data
+}
--- frontend/mall/src/pages/todo/ModifyPage.js
+++ frontend/mall/src/pages/todo/ModifyPage.js
@@ -1,17 +1,16 @@
-import { useNavigate } from "react-router-dom";
-
-const ModifyPage = ({tno})=>{
-    const navigate = useNavigate()
-    const moveToRead=()=>{
-        navigate({pathname:`/todo/read/${tno}`})
-    }
-    const moveToList = ()=>{
-        navigate({pathname:`/todo/list`})
-    }
+import { useParams } from "react-router-dom";
+import ModifyComponent from "../../components/todo/ModifyComponent";
+// 매개변수 {tno} : component로 이동
+const ModifyPage = ()=>{
+    // navigate : useCustomMove로 이동
+    const {tno} = useParams()   //component 전달위한 추가
     return(
-    <div className="text-3xl font-extrabold">
-        Todo Modify Page
-    </div>    
+        <div className="p-4 w-full bg-white">
+            <div className="text-3xl font-extrabold">
+            Todo Modify Page
+            </div>
+            <ModifyComponent tno={tno}/>
+        </div>        
     );
 }
 
